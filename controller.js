8ca59e63c5b843d41c0ca076addb0745bb4fc804
Austin Walker <Austin Walker>
2016-09-02 16:17:06 -0600

    W3D4 Mini-project

--- controller.js
+++ controller.js
@@ -1,10 +1,23 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+  $scope.pageNum = 1;
   $scope.getUsers = function() {
-    mainService.getUsers().then(function(dataFromService) {
-      $scope.users = dataFromService.data.data;
+    mainService.getUsers($scope.pageNum).then(function(dataFromService) {
+      $scope.users = dataFromService.data;
+      $scope.totalPages = dataFromService.total_pages;
     });
-  }
+  };
 
   $scope.getUsers();
 
+  $scope.prev = function () {
+    if ($scope.pageNum > 1) {
+      $scope.pageNum--;
+      $scope.getUsers();
+  }};
+
+  $scope.next = function() {
+    if ($scope.pageNum < $scope.totalPages) {
+      $scope.pageNum++;
+      $scope.getUsers();
+  }};
 });
--- service.js
+++ service.js
@@ -1,10 +1,18 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
-  this.getUsers = function() {
-    return $http({
+  this.getUsers = function(pageNum) {
+    var deferred =$q.defer();
+    $http({
         method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+        url: 'http://reqres.in/api/users?page=' + pageNum
+    }).then(function(response){
+      var parsedResponse = response.data;
+      for(var i =0; i < parsedResponse.length; i++){
+        parsedResponse[i].first_name = 'Ralf';
+      }
+      deferred.resolve(parsedResponse);
+    });
+    return deferred.promise;
+  };
 
 });
